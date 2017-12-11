e7f776ffac7cd320e723ac4dd598ce1eb2514ee9
alexhoma <alexcm.14@gmail.com>
2017-12-11 13:43:53 +0100

    wip(generator): generate the relative assets path of each file

--- bin/docgen.js
+++ bin/docgen.js
@@ -1,35 +1,28 @@
-const fileToString = require("./helpers").fileToString;
-const parseMarkdownFile = require("./helpers").parseMarkdownFile;
-
 const hogan = require("hogan.js");
 const fsPath = require("fs-path");
 const path = require("path");
 const walk = require("walk");
 
+const fileToString = require("./helpers").fileToString;
+const parseMarkdownFile = require("./helpers").parseMarkdownFile;
 
+/**
+ * Constants
+ */
 const CONTENT_DIR = path.resolve(__dirname, '../src/content');
 const TEMPLATES_DIR = path.resolve(__dirname, '../src/templates');
 const DIST_DIR = path.resolve(__dirname, '../docs');
 
-/**
- * Get resources tree
- */
-function main() {
-    let walker = walk.walk(CONTENT_DIR, {
-        followLinks: true
-    });
-
-    walker.on('file', function (root, stat, next) {
-        renderCompiledTemplateIntoHTMLFile({
-            name: stat.name,
-            path: root + '/' + stat.name
-        });
-
-        next();
-    });
-}
 
 const compileFileTemplate = function(file) {
+    let arrayFilePath = file.path
+        .replace(CONTENT_DIR, "")
+        .split("/");
+    let trimmedFilePath = arrayFilePath.slice(1, arrayFilePath.length);
+    let assetsRelativePath = trimmedFilePath.map(function() {
+        // WIP
+    });
+    console.log(assetsRelativePath);
     /**
      * Get markdown and parse its content
      */
@@ -47,13 +40,15 @@ const compileFileTemplate = function(file) {
     /**
      * Rendered template with the content
      */
-    return template.render(parsedFile);
+    return template.render({
+        ...parsedFile
+    });
 };
 
 /**
  * Write File on directory
  */
-const renderCompiledTemplateIntoHTMLFile = function(file) {
+const createDocFile = function(file) {
     let targetFile = file.path
         .replace(CONTENT_DIR, DIST_DIR)
         .replace('.md', '.html');
@@ -73,6 +68,19 @@ const renderCompiledTemplateIntoHTMLFile = function(file) {
 };
 
 /**
- * Exec
+ * Main executable function
  */
-main();
+(function () {
+    let walker = walk.walk(CONTENT_DIR, {
+        followLinks: true
+    });
+
+    walker.on('file', function (root, stat, next) {
+        createDocFile({
+            name: stat.name,
+            path: root + '/' + stat.name
+        });
+
+        next();
+    });
+})();
