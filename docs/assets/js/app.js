3cb7c1059e102bca874e46596e0838b444590610
alexhoma <alexcm.14@gmail.com>
2017-12-15 11:23:13 +0100

    feat(jsFiles): toggle sidebar menu on mobile versions

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -72,6 +72,21 @@
 
 __webpack_require__(1);
 
+/**
+ * Menu left toggle
+ */
+document.querySelector('#burgerSidebarMenu').addEventListener('click', function () {
+    var menu = document.querySelector('#sidebarMenu');
+
+    if (menu.className.indexOf('d-block') === -1) {
+        menu.classList.add('d-block');
+        menu.classList.remove('d-none');
+    } else {
+        menu.classList.add('d-none');
+        menu.classList.remove('d-block');
+    }
+});
+
 /***/ }),
 /* 1 */
 /***/ (function(module, exports) {
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 import "../styles/index.sass";
+
+
+/**
+ * Menu left toggle
+ */
+document
+    .querySelector('#burgerSidebarMenu')
+    .addEventListener('click', () => {
+        let menu = document.querySelector('#sidebarMenu');
+
+        if (menu.className.indexOf('d-block') === -1) {
+            menu.classList.add('d-block');
+            menu.classList.remove('d-none');
+        } else {
+            menu.classList.add('d-none');
+            menu.classList.remove('d-block');
+        }
+    });
